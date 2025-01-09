421dd340d4f7a9bf28b2e096e446f2715b6fd65f
sathish-oaza <sathish@oaza.energy>
2025-01-09 10:04:07 -0500

    Update LiveKit sample

---
+++ agent/agent.py
@@ -0,0 +1,79 @@
+import os
+import logging
+
+from dotenv import load_dotenv
+from livekit.agents import (
+    AutoSubscribe,
+    JobContext,
+    JobProcess,
+    WorkerOptions,
+    cli,
+    llm,
+)
+from livekit.agents.pipeline import VoicePipelineAgent
+from livekit.plugins import openai, deepgram, silero, azure
+
+
+load_dotenv(dotenv_path=".env.local")
+logger = logging.getLogger("voice-agent")
+
+
+def prewarm(proc: JobProcess):
+    proc.userdata["vad"] = silero.VAD.load()
+
+
+async def entrypoint(ctx: JobContext):
+    initial_ctx = llm.ChatContext().append(
+        role="system",
+        text=(
+            "You are a voice assistant created by LiveKit. Your interface with users will be voice. "
+            "You should use short and concise responses, and avoiding usage of unpronouncable punctuation. "
+            "You were created as a demo to showcase the capabilities of LiveKit's agents framework."
+        ),
+    )
+
+    logger.info(f"connecting to room {ctx.room.name}")
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+
+    # Wait for the first participant to connect
+    participant = await ctx.wait_for_participant()
+    logger.info(f"starting voice assistant for participant {participant.identity}")
+
+    # This project is configured to use Azure STT, OpenAI LLM and TTS plugins
+    # Learn more and pick the best one for your app:
+    # https://docs.livekit.io/agents/plugins
+    agent = VoicePipelineAgent(
+        vad=ctx.proc.userdata["vad"],
+        stt=azure.STT(
+            speech_key=os.getenv("AZURE_SPEECH_KEY"),
+            speech_region=os.getenv("AZURE_SPEECH_REGION"),
+            speech_host=os.getenv("AZURE_SPEECH_HOST")
+        ),
+        llm=openai.LLM.with_azure(
+            model=os.getenv("OPENAI_MODEL"),
+            azure_endpoint=os.getenv("AZURE_ENDPOINT"),
+            azure_deployment=os.getenv("AZURE_DEPLOYMENT"),
+            api_version=os.getenv("AZURE_API_VERSION"),
+            api_key=os.getenv("AZURE_API_KEY")
+        ),
+        tts=azure.TTS(
+            speech_key=os.getenv("AZURE_SPEECH_KEY"),
+            speech_region=os.getenv("AZURE_SPEECH_REGION"),
+            speech_host=os.getenv("AZURE_TTS_HOST")
+        ),
+        chat_ctx=initial_ctx,
+    )
+
+    agent.start(ctx.room, participant)
+
+    # The agent should be polite and greet the user when it joins :)
+    await agent.say("Hey, how can I help you today?", allow_interruptions=True)
+
+
+if __name__ == "__main__":
+    cli.run_app(
+        WorkerOptions(
+            entrypoint_fnc=entrypoint,
+            prewarm_fnc=prewarm,
+        ),
+    )
